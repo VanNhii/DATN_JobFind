2664b7eca4def209819ca3d160c21b55998be425
VanNhii <nhivodmx2003@gmail.com>
2025-12-05 08:40:06 +0700

    Fix merge conflict server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,38 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+
+// Kết nối DB
 const connectDB = require("./config/database");
+
+// Middlewares
 const errorHandler = require("./middlewares/errorHandler");
+
+// Routes
+const authRoutes = require("./routes/authRoutes");
+const recruiterRoutes = require("./routes/recruiterRoutes");
+const userRoutes = require("./routes/userRoutes");
 const candidateRoutes = require("./routes/candidateRoutes");
+
 const app = express();
+
 // 2. Kết nối DB
 connectDB();
 
 // 3. Middleware cơ bản
 app.use(cors({ origin: "*", credentials: true }));
-app.use(express.json()); // Quan trọng nhất để nhận email/pass
+app.use(express.json()); // Quan trọng để nhận JSON từ client
 
-// 4. Routes
-// Mọi request bắt đầu bằng /api/v1/auth sẽ chui vào authRoutes xử lý
-
+// 4. Routes API
+app.use(`/api/${process.env.API_VERSION}/auth`, authRoutes);
+app.use(`/api/${process.env.API_VERSION}/recruiters`, recruiterRoutes);
+app.use(`/api/${process.env.API_VERSION}/users`, userRoutes);
 app.use(`/api/${process.env.API_VERSION}/candidates`, candidateRoutes);
 
-// 5. Xử lý lỗi (Nên có để biết tại sao code lỗi)
+// 5. Xử lý lỗi chung
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server Auth đang chạy trên port ${PORT}`);
+  console.log(`Server đang chạy trên port ${PORT}`);
 });
